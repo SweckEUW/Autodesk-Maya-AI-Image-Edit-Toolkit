3498d91f321d95357efd73395e2f5b4d229e2718
Simon Weck <Simon.weck@raumtaenzer.com>
2022-07-05 04:14:45 +0200

    fixed super resolution bug

--- scripts/image_super_resolution/model/srgan.py
+++ scripts/image_super_resolution/model/srgan.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.layers import Add, BatchNormalization, Conv2D, Dense, Flatten, Input, LeakyReLU, PReLU, Lambda
 
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.applications.vgg19 import VGG19
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.vgg19 import VGG19
 
 from image_super_resolution.model.common import pixel_shuffle, normalize_01, normalize_m11, denormalize_m11
 
